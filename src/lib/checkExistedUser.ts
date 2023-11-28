4753c81edda35b29a19e158d6236fac79287d4e9
Tri Thien Nguyen <anretrithien@gmail.com>
2023-11-28 11:46:48 +0000

    Fix Parameter 'user' has an 'any' type

--- src/lib/checkExistedUser.ts
+++ src/lib/checkExistedUser.ts
@@ -1,11 +1,14 @@
 import { auth } from "@clerk/nextjs";
 
+interface User {
+  user_id: string;
+}
 
 const { userId } = await auth()
 
 export async function getServerSideProps() {
     const res = await fetch(`${process.env.NEXTAUTH_URL}/api/users`);
-    const data = await res.json();
+    const data: User[] = await res.json();
     const userIds = data.map(user => user.user_id);
     const answered = userIds.includes(userId);
     return answered
